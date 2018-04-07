ff087073b4f4aea2bc74f94e7aeb1755ce8a9564
sythemedia <36555749+sythemedia@users.noreply.github.com>
2018-04-07 14:23:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" id="pee">
 <head>
-<title>RBXLTHUNDER</title>
+<title>RBLXTHUNDER</title>
 <link rel="stylesheet" type="text/css" href="style.css" title="normal"/>
 <link rel="Shortcut Icon" type="image/ico" href="images/WinServer.ico"/>
 <script type="text/javascript" src="js/js_funcs.js"></script>
@@ -10,7 +10,7 @@
 <div id="Container">
 				<div id="Header">
 					<div id="Banner">
-						<center><div id="Logo"><a id="logo" title="RBXLegacy" href="index.php" style="display:inline-block;cursor:pointer;"><img src="images/6DCCBF27-4C5D-485D-8D03-BB5D815D078A.png" border="0" id="img" alt="Origins06"/></a></div></center>
+						<center><div id="Logo"><a id="logo" title="RBLX" href="index.php" style="display:inline-block;cursor:pointer;"><img src="images/6DCCBF27-4C5D-485D-8D03-BB5D815D078A.png" border="0" id="img" alt="Origins06"/></a></div></center>
 					</div>
 					<div class="Navigation">
 						<span><a id="Servers" class="MenuItem" href="servers.php">Servers</a></span>
@@ -22,7 +22,7 @@
 	<div id="SplashContainer">
 		<div id="MainPanel">
 			<center>
-			<h2>Welcome to RBXLThunder!</h2>
+			<h2>Welcome to RBlXThunder!</h2>
 
             <h3>WE ARE IN BETA TESTING!</h3>
 			<img src="images/rbxlegacymain.png" width="480" height="360" frameborder="0">
