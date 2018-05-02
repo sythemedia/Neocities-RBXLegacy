740c738c2f40b82ece25c19a13cf83f63abdbfb5
sythemedia <36555749+sythemedia@users.noreply.github.com>
2018-05-01 21:28:18 -0400

    Create index.php

---
+++ catalog/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" id="pee">
+<head>
+<title>THUNDER - CATALOG</title>
+<link rel="stylesheet" type="text/css" href="style.css" title="normal"/>
+<link rel="Shortcut Icon" type="image/ico" href="images/WinServer.ico"/>
+<script type="text/javascript" src="js/js_funcs.js"></script>
+</head>
+<body onload="set_style_from_cookie()">
+<div id="Container">
+				<div id="Header">
+					<div id="Banner">
+						<center><div id="Logo"><a id="logo" title="RBLX" href="index.php" style="display:inline-block;cursor:pointer;"><img src="images/6DCCBF27-4C5D-485D-8D03-BB5D815D078A.png" border="0" id="img" alt="Origins06"/></a></div></center>
+					</div>
+					<div class="Navigation">
+						<span><a id="Servers" class="MenuItem" href="servers.php">Servers</a></span>
+						
+ 					</div>
+				</div>
+				<div id="Body">
+					
+	<div id="SplashContainer">
+		<div id="MainPanel">
+			<center>
+			<h2>Welcome to the CATALOG!!</h2>
+			</center>
+		</div>
+	</div>
+	</div>
+	
+			</div>
+	</body>
+</html>
